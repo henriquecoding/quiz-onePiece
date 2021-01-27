a7383583fdb2f11f797766584ad5992ce034302d
henpassquesori <74831674+henpassquesori@users.noreply.github.com>
2021-01-27 13:52:28 +0000

    Adicionando Input
    Input para inserção de nome

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,13 @@
+/* eslint-disable react/jsx-indent-props */
+/* eslint-disable no-trailing-spaces */
+import React, { useState } from 'react';
 import styled from 'styled-components';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+
 import db from '../db.json';
 import Widget from '../src/components/Widget';
+import QuizLogo from '../src/components/QuizLogo';
 import QuizBackground from '../src/components/QuizBackground';
 import Footer from '../src/components/Footer';
 import GitHubCorner from '../src/components/GitHubCorner';
@@ -17,29 +24,52 @@ export const QuizContainer = styled.section`
 `;
 
 export default function Home() {
-  return (
-  <QuizBackground backgroundImage = {db.bg}>
-    <QuizContainer>
-      <Widget>
-        <Widget.Header>
-          <h1>One Piece</h1>
-        </Widget.Header>
-        <Widget.Content>
-          <p>akishdipsndocmaslclçsmaçmxlamscas</p>
-        </Widget.Content>
-      </Widget>  
+  const router = useRouter();
+  const [name, setName] = React.useState('');
 
-      <Widget>
-        <Widget.Header>
-          <h1>Quizes da Galera</h1>
-        </Widget.Header>
-        <Widget.Content>
-          <p>akishdipsndocmaslclçsmaçmxlamscas</p>
-        </Widget.Content>
-      </Widget>
-      <Footer />
-    </QuizContainer>
-    <GitHubCorner projectUrl = 'https://github.com/henpassquesori'/>
-  </QuizBackground>
-    );
+  return (
+    <QuizBackground backgroundImage={db.bg}>
+      <Head>
+        <title>One Piece - Quiz</title>
+        <meta property="og:image" content="" />
+        <link rel="shortcut icon" href="../img/favicon.ico" type="image/x-icon" />
+      </Head>
+      <QuizContainer>
+        <QuizLogo />
+        <Widget>
+          <Widget.Header>
+            <h1>One Piece</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <form onSubmit={function (infosDoEvento) {
+              infosDoEvento.preventDefault();
+              router.push(`/quiz?name=${name}`);
+            }}>
+              <input 
+              onChange={function (infosDoEvento) {
+                setName(infosDoEvento.target.value);
+              }}
+              placeholder="Digite seu nome..." 
+              />
+            </form>
+            <button type="submit" disabled={name.length === 0}>
+              Jogar 
+              {name}
+            </button>
+          </Widget.Content>
+        </Widget>
+
+        <Widget>
+          <Widget.Header>
+            <h1>Quizes da Galera</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <p>akishdipsndocmaslclçsmaçmxlamscas</p>
+          </Widget.Content>
+        </Widget>
+        <Footer />
+      </QuizContainer>
+      <GitHubCorner projectUrl="https://github.com/henpassquesori" />
+    </QuizBackground>
+  );
 }
---
+++ pages/quiz.js
@@ -0,0 +1,14 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable react/self-closing-comp */
+/* eslint-disable eol-last */
+/* eslint-disable semi */
+
+import React from 'react';
+
+export default function QuizPage() {
+  return (
+    <section>
+      Página de Quiz
+    </section>
+  );
+}
