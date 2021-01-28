be4c0506bbba7b04e234e4fd4674b4d9df319ddf
henpassquesori <74831674+henpassquesori@users.noreply.github.com>
2021-01-28 16:57:16 +0000

    Melhorando Visualização
    Aumentei o tamanho das fontes para melhor visibilidade

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -9,10 +9,11 @@ import styled from 'styled-components';
 const InputBase = styled.input`
   width: 100%;
   padding: 15px;
-  font-size: 14px;
-  border: 1px solid ${({ theme }) => theme.colors.primary};
+  font-size: 20px;
+  font-weight: 700;
+  border: 2px solid ${({ theme }) => theme.colors.primary};
   color: ${({ theme }) => theme.colors.contrastText};
-  background-color: ${({ theme }) => theme.colors.mainBg};
+  background-color: ${({ theme }) => theme.colors.inputBg};
   border-radius: ${({ theme }) => theme.borderRadius};
   outline: 0;
   margin-bottom: 25px;
--- src/components/Widget/index.js
+++ src/components/Widget/index.js
@@ -8,13 +8,13 @@ const Widget = styled.div`
   border-radius: 4px;
   overflow: hidden;
   h1, h2, h3 {
-    font-size: 16px;
+    font-size: 30px;
     font-weight: 700;
     line-height: 1;
     margin-bottom: 0;
   }
   p {
-    font-size: 14px;
+    font-size: 20px;
     font-weight: 400;
     line-height: 1;
   }
